590a547adcedbdebec7c7a7086921f3478d8a47e
mitch <mitch@rice.edu>
2020-01-04 23:23:59 -0500

    dracula color scheme

--- config.def.h
+++ config.def.h
@@ -10,17 +10,17 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
-static const char col_gray1[]       = "#eeeeee";
+static const char *fonts[]          = { "monospace:size=18" };
+static const char dmenufont[]       = "monospace:size=18";
+static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#bbbbbb";
-static const char col_gray3[]       = "#444444";
-static const char col_gray4[]       = "#222222";
-static const char col_cyan[]        = "#b4f9a5";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#f8f8f2";
+static const char col_cyan[]        = "#ff79c6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
 };
 
 /* tagging */
